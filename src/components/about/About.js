f3c19966654da3b9db4860b511f817c87b0a084b
Oshri Cohen <oshriakaa@gmail.com>
2021-06-30 23:24:02 +0300

    change about

--- src/components/about/About.js
+++ src/components/about/About.js
@@ -6,11 +6,13 @@ const About = () => {
   return (
     <div className="about-container" id="about-me">
       <div className="about-information">
-        <h2>About</h2>
+        <h2 className="about-title">About</h2>
+        <p>Hello, My name is Oshri Cohen.</p>
         <p>
-          Hello, My name is Oshri Cohen. I am a Software Developer with passion
-          for coding, a lot of motivation, self learning and group player!
+          I am a Software Developer with passion for coding, a lot of
+          motivation, self learning and group player!
         </p>
+        <p>HIT graduate with B.Sc in Computer Science.</p>
       </div>
       <div className="headshot-container">
         <img src="../images/oshripic.jpg" alt="oshri-pic" />
