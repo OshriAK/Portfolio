0c7ca30de727c26b43f11001bb6316040bde89e3
Oshri Cohen <oshriakaa@gmail.com>
2021-07-04 10:13:02 +0300

    change cv to pdf

--- src/components/hero/Hero.js
+++ src/components/hero/Hero.js
@@ -46,7 +46,7 @@ const Hero = () => {
           <h1 className="hero-my-name">Oshri Cohen</h1>
           <p>Full Stack Developer</p>
           <div className="hero-button">
-            <a href="../cv/oshri-cv.pdf" download="oshri-cv.pdf">
+            <a href="../cv/oshri-cohen-CV.pdf" download="oshri-cohen-CV.pdf">
               Download CV
             </a>
           </div>
